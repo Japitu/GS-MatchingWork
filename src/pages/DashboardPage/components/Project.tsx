da758a5feb789a534fd873d3561a9d2903a58e03
Felipe Ishii <felipeishii55@gmail.com>
2025-11-23 19:15:21 -0300

    Feature/login

--- src/pages/DashboardPage/components/Project.tsx
+++ src/pages/DashboardPage/components/Project.tsx
@@ -86,7 +86,6 @@ const Project = () => {
 
   return (
     <div className={`min-h-screen w-full max-w-7xl mx-auto p-4 md:p-6 flex flex-col ${darkMode ? "bg-gray-900 text-gray-100" : "bg-gray-50"}`}>
-      
       <div className="md:ml-64">
         <h2 className="text-2xl font-semibold mb-6">Projetos / Empresas</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
